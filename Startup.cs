5c310b0c2b1b68ecab314e0593f194c89ed8b79a
Guilherme Teixeira Valota <guilhermevalota@credisan.com.br>
2024-05-14 13:32:21 -0300

    Ajuste: Removido do LocalHost e usando o IPV4 da maquina

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,7 @@
-﻿namespace WebAPIArenaCactus
+﻿using Microsoft.OpenApi.Models;
+using System.Net;
+
+namespace WebAPIArenaCactus
 {
     public class Startup
     {
@@ -7,10 +10,7 @@
             Configuration = configuration;
         }
 
-        public String ConnString = "";
-
         public IConfiguration Configuration { get; }
-
         public void ConfigureServices(IServiceCollection services)
         {
             IConfigurationRoot configuration = new ConfigurationBuilder()
@@ -18,9 +18,54 @@
            .AddJsonFile("appsettings.json")
            .Build();
 
+           services.AddControllers();
+
+            // Configurando o CORS
+            services.AddCors(options =>
+            {
+                options.AddPolicy("_myAllowSpecificOrigins",
+                    builder =>
+                    {
+                        builder.WithOrigins("http://*.*",
+                                            "https://*.*")
+                               .AllowAnyHeader()
+                               .AllowAnyMethod()
+                               .AllowAnyOrigin();
+                    });
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
             configuration.GetConnectionString("MySqlDbConnection");
 
             services.AddSingleton<IConfiguration>(Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ArenaCaCtus V1");
+                });
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
